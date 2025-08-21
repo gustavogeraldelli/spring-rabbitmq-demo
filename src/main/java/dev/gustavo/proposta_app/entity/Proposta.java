b67c8c4fadf47960ae63e6e7406609d688e4557d
gustavo <geraldellig@gmail.com>
2025-08-20 23:29:49 -0300

    feat: entities

---
+++ src/main/java/dev/gustavo/proposta_app/entity/Proposta.java
@@ -0,0 +1,27 @@
+package dev.gustavo.proposta_app.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Proposta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Double valorSolicitado;
+    private Integer prazo;
+    private Boolean aprovada;
+    private Boolean integrada;
+    private String observacao;
+    @OneToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+}
