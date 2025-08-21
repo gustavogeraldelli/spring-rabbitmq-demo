f77a2c632609adf14afbd89d648d9ac29959290b
gustavo <geraldellig@gmail.com>
2025-08-21 18:58:46 -0300

    feat: scheduled task to retry failed messages

--- src/main/java/dev/gustavo/proposta_app/repository/PropostaRepository.java
+++ src/main/java/dev/gustavo/proposta_app/repository/PropostaRepository.java
@@ -3,5 +3,10 @@ package dev.gustavo.proposta_app.repository;
 import dev.gustavo.proposta_app.entity.Proposta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PropostaRepository extends JpaRepository<Proposta, Long> {
+
+    List<Proposta> findByIntegradaIsFalse();
+
 }
---
+++ src/main/java/dev/gustavo/proposta_app/scheduler/PropostasNaoIntegradas.java
@@ -0,0 +1,37 @@
+package dev.gustavo.proposta_app.scheduler;
+
+import dev.gustavo.proposta_app.repository.PropostaRepository;
+import dev.gustavo.proposta_app.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@RequiredArgsConstructor
+@Component
+public class PropostasNaoIntegradas {
+
+    private final PropostaRepository propostaRepository;
+    private final NotificationService notificationService;
+
+    private final Logger logger = LoggerFactory.getLogger(PropostasNaoIntegradas.class);
+
+    // tentar enviar novamente para a fila
+    @Scheduled(fixedDelay = 30, timeUnit = TimeUnit.SECONDS)
+    public void buscarPropostasQueNaoForamIntegradas() {
+        propostaRepository.findByIntegradaIsFalse().forEach(proposta -> {
+           try {
+               notificationService.notificar(proposta, "proposta-pendente.ex");
+               proposta.setIntegrada(true);
+               propostaRepository.save(proposta);
+           }
+           catch (RuntimeException e) {
+               logger.error(e.getMessage());
+           }
+        });
+    }
+
+}
