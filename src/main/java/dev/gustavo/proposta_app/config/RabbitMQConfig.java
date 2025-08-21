d2571c8b7bb40bcd2bfdc24ced231a2af5309336
gustavo <geraldellig@gmail.com>
2025-08-21 13:05:20 -0300

    feat: implementing RabbitTemplate json message converter

--- src/main/java/dev/gustavo/proposta_app/config/RabbitMQConfig.java
+++ src/main/java/dev/gustavo/proposta_app/config/RabbitMQConfig.java
@@ -3,6 +3,8 @@ package dev.gustavo.proposta_app.config;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
@@ -20,6 +22,13 @@ public class RabbitMQConfig {
     @Bean
     public ApplicationListener<ApplicationReadyEvent> initialize(RabbitAdmin rabbitAdmin) {
         return event -> rabbitAdmin.initialize();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+        return rabbitTemplate;
     }
 
     /**
