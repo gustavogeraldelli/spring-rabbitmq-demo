c50264addfe73f590469fd6e49db71ab5409333c
gustavo <geraldellig@gmail.com>
2025-08-21 19:42:00 -0300

    feat(notificacao): initializr

---
+++ notificacao-ms/src/main/java/dev/gustavo/notificacao_ms/NotificacaoMsApplication.java
@@ -0,0 +1,13 @@
+package dev.gustavo.notificacao_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificacaoMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificacaoMsApplication.class, args);
+	}
+
+}
