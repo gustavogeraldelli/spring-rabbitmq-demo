754cc4daabce77035b30bd382ea2bd4bffc740c2
gustavo <geraldellig@gmail.com>
2025-08-22 18:40:39 -0300

    feat(notificacao): novo listener para nova fila do analisar-credito-ms

---
+++ ms-notificacao/src/main/java/dev/gustavo/notificacao_ms/listener/PropostaConcluidaListener.java
@@ -0,0 +1,25 @@
+package dev.gustavo.notificacao_ms.listener;
+
+import dev.gustavo.notificacao_ms.domain.Proposta;
+import dev.gustavo.notificacao_ms.service.SNSService;
+import dev.gustavo.notificacao_ms.utils.Mensagens;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class PropostaConcluidaListener {
+
+    private final SNSService snsService;
+
+    @RabbitListener(queues = "${rabbitmq.queue.proposta.pendente}")
+    public void propostaPendente(Proposta proposta) {
+        String msg = "";
+        if (proposta.getAprovada()) msg = Mensagens.PROPOSTA_APROVADA;
+        else msg = Mensagens.PROPOSTA_NEGADA;
+        String fmt = String.format(msg, proposta.getUsuario().getNome());
+        snsService.notificar(proposta.getUsuario().getTelefone(), fmt);
+    }
+
+}
--- ms-notificacao/src/main/java/dev/gustavo/notificacao_ms/utils/Mensagens.java
+++ ms-notificacao/src/main/java/dev/gustavo/notificacao_ms/utils/Mensagens.java
@@ -3,5 +3,7 @@ package dev.gustavo.notificacao_ms.utils;
 public class Mensagens {
 
     public static final String PROPOSTA_EM_ANALISE = "%s, sua proposta foi recebida e está em analise";
+    public static final String PROPOSTA_NEGADA = "%s, sua proposta foi negada";
+    public static final String PROPOSTA_APROVADA = "%s, sua proposta foi aprovada";
 
 }
