744627862b743cd7919133678b663953d3eeef1f
gustavo <geraldellig@gmail.com>
2025-08-22 16:10:46 -0300

    feat(analise): initializr

---
+++ analise-credito/src/main/java/dev/gustavo/analise_credito/AnaliseCreditoApplication.java
@@ -0,0 +1,13 @@
+package dev.gustavo.analise_credito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnaliseCreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnaliseCreditoApplication.class, args);
+	}
+
+}
